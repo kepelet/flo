28374f9297646c7538f1aa5ceef7504e6c1ae4bd
rizaldy <oss@rizaldy.club>
2024-06-24 20:16:13 +0700

    refactor: move NowPlaying to Models

---
+++ flo/Shared/Models/NowPlaying.swift
@@ -0,0 +1,18 @@
+//
+//  NowPlaying.swift
+//  flo
+//
+//  Created by rizaldy on 24/06/24.
+//
+
+import Foundation
+
+struct NowPlaying: Encodable, Decodable {
+  var artistName: String = "Unknown Artist"
+  var songName: String = "Untitled"
+  var albumName: String = "Unknown Album"
+  var albumCover: String = ""
+  var streamUrl: String = ""
+  var bitRate: Int = 0
+  var suffix: String = ""
+}
