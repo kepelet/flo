173ef992dec790b717e559f9512d79f3479bd7c3
rizaldy <oss@rizaldy.club>
2024-06-24 17:01:25 +0700

    feat: add AVAudioSession

--- flo/App.swift
+++ flo/App.swift
@@ -5,10 +5,19 @@
 //  Created by rizaldy on 01/06/24.
 //
 
+import AVFoundation
 import SwiftUI
 
 @main
 struct FloApp: App {
+  init() {
+    do {
+      try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+      try AVAudioSession.sharedInstance().setActive(true)
+    } catch {
+      print(error)
+    }
+  }
 
   var body: some Scene {
     WindowGroup {
