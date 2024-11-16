46bd978f9f06f9ff5471f5f654c2587331bf03a1
rizaldy <oss@rizaldy.club>
2024-11-17 01:31:33 +0700

    feat: add ArtistView

---
+++ flo/ArtistsView.swift
@@ -0,0 +1,71 @@
+//
+//  ArtistsView.swift
+//  flo
+//
+//  Created by rizaldy on 30/10/24.
+//
+
+import SwiftUI
+
+struct ArtistsView: View {
+  @EnvironmentObject private var viewModel: AlbumViewModel
+
+  @State private var searchArtist = ""
+
+  let artists: [Artist]
+
+  var filteredArtists: [Artist] {
+    if searchArtist.isEmpty {
+      return artists
+    } else {
+      return artists.filter { artist in
+        artist.name.localizedCaseInsensitiveContains(searchArtist)
+          || artist.fullText.localizedCaseInsensitiveContains(searchArtist)
+      }
+    }
+  }
+
+  var body: some View {
+    NavigationStack {
+      ScrollView {
+        LazyVStack {
+          ForEach(filteredArtists) { artist in
+            NavigationLink {
+              ArtistDetailView(artist: artist)
+                .environmentObject(viewModel)
+            } label: {
+              VStack {
+                HStack {
+                  Text(artist.name)
+                    .customFont(.headline)
+
+                  Spacer()
+
+                  Image(systemName: "chevron.right")
+                    .foregroundColor(.gray)
+                    .font(.caption)
+                }
+                .padding(.horizontal)
+                .padding(.vertical, 5)
+
+                Rectangle()
+                  .frame(height: 0.5)
+                  .foregroundColor(.gray.opacity(0.5))
+              }
+            }
+          }
+        }.padding(.bottom, 100)
+      }
+      .navigationTitle("Artists")
+      .searchable(
+        text: $searchArtist, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search"
+      )
+    }
+  }
+}
+
+struct ArtistsView_Previews: PreviewProvider {
+  static var previews: some View {
+    ArtistsView(artists: [])
+  }
+}
