b84b77c77f826a1473646c7697c246fc0b2bbf13
rizaldy <oss@rizaldy.club>
2024-11-28 02:13:01 +0700

    fix: delete separated User models
    and do something related with #9

--- flo/Shared/Models/UserAuth.swift
+++ flo/Shared/Models/UserAuth.swift
@@ -16,4 +16,31 @@ struct UserAuth: Codable {
   let subsonicSalt: String
   let subsonicToken: String
   let token: String
+
+  init(
+    id: String, username: String, name: String, isAdmin: Bool, lastFMApiKey: String = "",
+    subsonicSalt: String = "", subsonicToken: String = "", token: String = ""
+  ) {
+    self.id = id
+    self.name = name
+    self.username = username
+    self.isAdmin = isAdmin
+    self.lastFMApiKey = lastFMApiKey
+    self.subsonicSalt = subsonicSalt
+    self.subsonicToken = subsonicToken
+    self.token = token
+  }
+
+  init(from decoder: any Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    self.id = try container.decode(String.self, forKey: .id)
+    self.name = try container.decode(String.self, forKey: .name)
+    self.username = try container.decode(String.self, forKey: .username)
+    self.isAdmin = try container.decode(Bool.self, forKey: .isAdmin)
+    self.lastFMApiKey = try container.decodeIfPresent(String.self, forKey: .lastFMApiKey) ?? ""
+    self.subsonicSalt = try container.decode(String.self, forKey: .subsonicSalt)
+    self.subsonicToken = try container.decode(String.self, forKey: .subsonicToken)
+    self.token = try container.decode(String.self, forKey: .token)
+  }
 }
