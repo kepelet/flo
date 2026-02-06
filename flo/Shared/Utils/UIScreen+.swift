907dfe7ecb02d75bc5852cf49e64a1a4718e9325
Francesco <s.f.longobardi@gmail.com>
2026-02-06 19:17:02 +0100

    fix: crash on smaller screen + scale factor for strings in stat card

---
+++ flo/Shared/Utils/UIScreen+.swift
@@ -0,0 +1,10 @@
+//    flo
+
+import SwiftUI
+
+extension UIScreen {
+  static let screenWidth = UIScreen.main.bounds.size.width
+  static let screenHeight = UIScreen.main.bounds.size.height
+  static let screenSize = UIScreen.main.bounds.size
+}
+
