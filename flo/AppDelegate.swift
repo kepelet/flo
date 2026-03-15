9806a9095aa121ef9f17e69cf8bf810004950b1d
rizaldy <oss@rizaldy.club>
2026-03-15 20:04:16 +0700

    feat: add AppDelegate and SceneDelegate for CarPlay support

---
+++ flo/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  flo
+//
+
+import AVFoundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+        } catch {
+            print(error)
+        }
+
+        #if os(iOS)
+        WatchConnectivityManager.shared.start()
+        #endif
+
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        // Return configuration for window scene to use SceneDelegate
+        let configuration = UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
+    }
+}
